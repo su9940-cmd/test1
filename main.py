feb26eac39813d4c8231690951e516da6d8a3843
JasonKim <su9940@gmail.com>
2026-07-16 12:35:40 +0900

    fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from multiplier import multiply1
 from devider import devide1
 
 
-# print('더하기: 'add1(1,2))
-# print('빼기: 'subtract1(3,1))
-print("곱하기: "+multiply1(2,3))
-print('나누기: '+devide1(4,2))
+# print('더하기: ',add1(1,2))
+# print('빼기: ',subtract1(3,1))
+print("곱하기: ",multiply1(2,3))
+print('나누기: ',devide1(4,2))
